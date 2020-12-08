49d9c67fb29d307eca7d9783477b5cb12fcd4f95
Vincent Wei <vincent.wei@etu.univ-paris-diderot.fr>
2020-12-08 21:45:19 +0100

    have to fix open Graphics

--- projet/turtle.ml
+++ projet/turtle.ml
@@ -1,3 +1,5 @@
+open Graphics
+open Stack
 
 type command =
 | Line of int
@@ -13,3 +15,29 @@ type position = {
 }
 
 (** Put here any type and function implementations concerning turtle *)
+
+(* New stack, initially empty *)
+let stackOfPos = create()
+
+
+let cordinateX p length = 
+  p.x +. (length  *. cos (float_of_int p.a))
+
+let cordinateY p length = 
+  p.y +. (length *. sin (float_of_int p.a))
+
+let cordinateXY p length =
+  let x1 = cordinateX p length and
+  y1 = cordinateY p length in
+  (x1, y1);;
+
+let draw_line pos a = 
+  let (x1, y1)  = cordinateXY pos a in 
+  lineto x1 y1;;
+
+
+let turtleToGraphics command pos = match command with
+  | Line a ->  draw_line pos (float_of_int a)
+  | Store -> push pos stackOfPos
+  | Restore -> pop stackOfPos
+
