b948f6922701a4282ee3f2e07100a40d065944af
Vincent Wei <vincent.wei@etu.univ-paris-diderot.fr>
2020-12-11 00:25:57 +0100

    add list_of_symb

--- projet/systems.ml
+++ projet/systems.ml
@@ -1,3 +1,4 @@
+open List
 (** Words, rewrite systems, and rewriting *)
 
 type 's word =
@@ -13,3 +14,30 @@ type 's system = {
     interp : 's -> Turtle.command list }
 
 (** Put here any type and function implementations concerning systems *)
+
+(**A[P[PA]A]**)
+
+(**
+Seq [Symb A; 
+  Branch (Seq [Symb P; 
+    Branch (Seq [Symb P; Symb A]); Symb A])]
+ **)
+
+(**function change string to char list
+ code from my project of Logique**)
+
+let explode s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
+
+(**function return sorted list of symb without replication**)
+let rec list_of_symb_loop accu sl = match sl with
+  | [] -> sort accu
+  | h :: t -> if mem h accu then list_of_symb_loop accu t else
+        list_of_symb_loop (h :: accu) t
+
+let list_of_symb sl = list_of_symb_loop []
+    (filter (fun x -> x <> '[' && x <> ']') sl)
+
+
