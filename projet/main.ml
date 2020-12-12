e8550c939b7e679d02429fb6af55d6f58fe9d1e0
ngof <felix.ngo@etu.univ-paris-diderot.fr>
2020-12-12 19:57:37 +0100

    read_file

--- projet/main.ml
+++ projet/main.ml
@@ -1,41 +1,71 @@
-open Graphics;;
-let _ = open_graph " 500x500"
+(* open Graphics;; *)
+
 open Lsystems (* Librairie regroupant le reste du code. Cf. fichier dune *)
 open Systems (* Par exemple *)
 open Turtle
+open Printf
+open Read
+
 
 
 (** Gestion des arguments de la ligne de commande.
     Nous suggérons l'utilisation du module Arg
     http://caml.inria.fr/pub/docs/manual-ocaml/libref/Arg.html
 *)
+(* let file = ref "." *)
+
+(* let setFile f = file := f; read_dir;; *)
+
+(* let read_file file =
+  let op = open_in file in
+  let rdLine = input_line op;in
+  let rec printall = print_endline rdLine; printall in
+  try
+    printall
+  with End_of_file -> flush stdout;
+  close_in op;;
+  (* with e -> close_in_noerr op;
+            raise e *) *)
+(* let read_file name =
+  let f = open_in name in
+  let rdLine () =
+    try Some (input_line f) with End_of_file -> None in
+  let rec loop () =
+    match rdLine () with
+    | Some s -> print_endline s; loop ()
+    | None -> flush stdout; close_in f in
+  loop () *)
+
 
 let usage = (* Entete du message d'aide pour --help *)
   "Interpretation de L-systemes et dessins fractals"
 
 let action_what () = Printf.printf "%s\n" usage; exit 0
 
+
+
+
 let cmdline_options = [
 ("--what" , Arg.Unit action_what, "description");
-("-f", Arg.String (read_dir), "lit le fichier de sauvegarde");
+("-f", Arg.String (read_file), "lit le fichier de sauvegarde");
 ]
 
 let extra_arg_action = fun s -> failwith ("Argument inconnu :"^s)
 
 let main () =
+  (* open_graph " 500x500"; *)
   Arg.parse cmdline_options extra_arg_action usage;
-  print_string "Pour l'instant je ne fais rien\n"
+  print_string "Bye\n"
 
 (** On ne lance ce main que dans le cas d'un programme autonome
     (c'est-à-dire que l'on est pas dans un "toplevel" ocaml interactif).
     Sinon c'est au programmeur de lancer ce qu'il veut *)
-let read_dir dir =
-  let _ = try open_out dir with Sys_error printf("Permission denied \n");
 
 
 let () = if not !Sys.interactive then main ()
 
-let rec loop t =
+(* keyStrokes listners *)
+(* let rec loop t =
   let event = wait_next_event [Key_pressed] in
   if event.keypressed
   then match event.key with
@@ -43,4 +73,4 @@ let rec loop t =
        | _    -> loop t
   else loop t
 
-let _ = loop 5
+let _ = loop 5 *)
---
+++ projet/read.mli
@@ -0,0 +1,7 @@
+val read_file : string -> unit
+
+val getAxiome : unit -> string
+
+val getRules : unit -> string list
+
+val getInter : unit -> string list
