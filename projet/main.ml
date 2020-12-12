e3bcc81f697e03b07e6cd6eba0503945ef77a2d4
ngof <felix.ngo@etu.univ-paris-diderot.fr>
2020-12-12 19:58:46 +0100

    bye comment

--- projet/main.ml
+++ projet/main.ml
@@ -12,30 +12,6 @@ open Read
     Nous suggérons l'utilisation du module Arg
     http://caml.inria.fr/pub/docs/manual-ocaml/libref/Arg.html
 *)
-(* let file = ref "." *)
-
-(* let setFile f = file := f; read_dir;; *)
-
-(* let read_file file =
-  let op = open_in file in
-  let rdLine = input_line op;in
-  let rec printall = print_endline rdLine; printall in
-  try
-    printall
-  with End_of_file -> flush stdout;
-  close_in op;;
-  (* with e -> close_in_noerr op;
-            raise e *) *)
-(* let read_file name =
-  let f = open_in name in
-  let rdLine () =
-    try Some (input_line f) with End_of_file -> None in
-  let rec loop () =
-    match rdLine () with
-    | Some s -> print_endline s; loop ()
-    | None -> flush stdout; close_in f in
-  loop () *)
-
 
 let usage = (* Entete du message d'aide pour --help *)
   "Interpretation de L-systemes et dessins fractals"
