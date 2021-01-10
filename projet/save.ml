27b4127dd8ec505cf542c841eca327977dc5e82f
ngof <felix.ngo@etu.univ-paris-diderot.fr>
2021-01-10 23:13:50 +0100

    ecriture de l'expression de base dans le fichier et check si l'expression ne contiens pas des symboles n'appartenant pas a l'ensemble de symbole donnee

--- projet/save.ml
+++ projet/save.ml
@@ -1,5 +1,6 @@
 open String
 open Sys
+open Systems
 
 let outputThenRead output =
   print_string output;
@@ -11,9 +12,34 @@ let rec newfileName () =
     (print_string "this file already exist, try a new name\n"; newfileName())
   else name
 
-(* cree un fichier donner avec un nom choisi *)
-let writeFile() =
-  let fname = newfileName() in
-  let newFile = open_out fname in
-  output_string newFile (String.concat " " ["###";fname;"\n"]);
-  close_out newFile
+let ensembleFini () =
+  let input = explode (outputThenRead "what is the symbol set :") in
+  let rec filter l acc =
+    match l with
+    |[] -> acc
+    |i :: l' -> if(i != ',') then filter l' (i::acc)
+                else filter l' acc in
+  print_string (String.of_seq (List.to_seq input));
+  filter input []
+
+let rec checkListValid l set =
+  match l with
+  |[] -> true
+  |i :: l' -> if(List.mem i set) then checkListValid l' set else false ;;
+
+let rec baseExpression set =
+  let input = outputThenRead "Base Expression: " in
+  let expression = explode input in
+  if checkListValid expression set then input
+    else (print_string "invalid Expression"; baseExpression set)
+
+  (* cree un fichier donner avec un nom choisi *)
+  let writeFile() =
+    let fname = newfileName() in
+    let newFile = open_out fname in
+    output_string newFile (String.concat " " ["###";fname;"\n"]);
+    let set = ensembleFini () in
+    let baseExp = baseExpression set in
+
+    output_string newFile (String.concat "" [baseExp;"\n"]);
+    close_out newFile
---
+++ projet/save.mli
@@ -0,0 +1,9 @@
+val outputThenRead: string -> string
+
+val newfileName : unit -> string
+
+val baseExpression : char list -> string
+
+val checkListValid : 'a list -> 'a list -> bool
+
+val writeFile : unit -> unit
