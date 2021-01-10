4d985a83ba52d49eae5ac0c6082dafc3f39663d4
ngof <felix.ngo@etu.univ-paris-diderot.fr>
2021-01-10 17:31:11 +0100

    fix fileName

--- projet/save.ml
+++ projet/save.ml
@@ -5,15 +5,15 @@ let outputThenRead output =
   print_string output;
   read_line ()
 
-let rec fileName () =
+let rec newfileName () =
   let name = outputThenRead "how would you like to call your file :" in
   if (Sys.file_exists name) then
-    (print_string "this file already exist, try a new name\n"; fileName())
+    (print_string "this file already exist, try a new name\n"; newfileName())
   else name
 
 (* cree un fichier donner avec un nom choisi *)
 let writeFile() =
-  let fname = fileName() in
+  let fname = newfileName() in
   let newFile = open_out fname in
   output_string newFile (String.concat " " ["###";fname;"\n"]);
   close_out newFile
