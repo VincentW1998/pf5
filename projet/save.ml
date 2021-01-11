8f985cc38105ca2aa00393f500520d3baa8de34e
ngof <felix.ngo@etu.univ-paris-diderot.fr>
2021-01-11 13:05:12 +0100

    creation de nouveau Lsys

--- projet/save.ml
+++ projet/save.ml
@@ -1,6 +1,16 @@
 open String
 open Sys
 open Systems
+open Str
+
+let fileName = ref "";;
+let newFile = ref stdout;;
+let symbolSet = ref [];;
+
+(* let rec charListToString l acc =
+  match l with
+  |[] -> acc
+  |c :: l' -> charListToString l' (String.concat "" [acc; String.make 1 c]) *)
 
 let outputThenRead output =
   print_string output;
@@ -12,34 +22,72 @@ let rec newfileName () =
     (print_string "this file already exist, try a new name\n"; newfileName())
   else name
 
+(* demande un ensemble fini de symbol pour le
+lsysteme et retourne la liste de char correspondant *)
 let ensembleFini () =
   let input = explode (outputThenRead "what is the symbol set :") in
+  (* retire les , de la liste *)
   let rec filter l acc =
     match l with
     |[] -> acc
     |i :: l' -> if(i != ',') then filter l' (i::acc)
                 else filter l' acc in
+
   print_string (String.of_seq (List.to_seq input));
+  print_string ("\n");
   filter input []
 
-let rec checkListValid l set =
+
+let rec checkListValid l =
   match l with
   |[] -> true
-  |i :: l' -> if(List.mem i set) then checkListValid l' set else false ;;
+  |i :: l' -> if(List.mem i !symbolSet) then checkListValid l' else false ;;
 
-let rec baseExpression set =
+let rec baseExpression () =
   let input = outputThenRead "Base Expression: " in
   let expression = explode input in
-  if checkListValid expression set then input
-    else (print_string "invalid Expression"; baseExpression set)
+  if checkListValid expression then input
+    else (print_string "invalid Expression"; baseExpression ())
+
+let rec interpretation s =
+  let input = outputThenRead (String.concat "" ["do you want to do something for ";String.make 1 s;" ? y/n:"]) in
+  match input with
+  |"n" -> ()
+  |"y" -> let interp = outputThenRead "what do you want to do ? : " in
+          if checkListValid (explode interp) then
+              output_string !newFile (String.concat " " [String.make 1 s ;interp; "\n"])
+          else (print_string "invalid interpretation\n"; interpretation s)
+  |_ -> failwith "wrong answer"
+
+let numeroValide n=
+    try int_of_string n |> ignore; true
+    with Failure _ -> false
+
+let rec askNumber () =
+  let input = outputThenRead "how much ? :" in
+  (* if(Str.string_match (Str.regexp "[0-9+-]+$") input 0) then input *)
+  if numeroValide input then input
+  else (print_string "invalid answer, please enter a number \n"; askNumber())
+
+
+let rec assignCharToCmd s =
+  let input = outputThenRead (String.concat " " ["Which cmd for ";String.make 1 s;" ? y/n:"]) in
+  match input with
+  | "line" -> output_string !newFile (String.concat "" [String.make 1 s ;" L"; askNumber () ; "\n"])
+  | "move" -> output_string !newFile (String.concat "" [String.make 1 s ;" M"; askNumber () ; "\n"])
+  | "turn" -> output_string !newFile (String.concat "" [String.make 1 s ;" T"; askNumber () ; "\n"])
+  | "Store" -> output_string !newFile (String.concat "" [String.make 1 s ;" S";"\n"])
+  | "Restore" -> output_string !newFile (String.concat "" [String.make 1 s ;" R";"\n"])
+  | _ -> (print_string "invalid command\n"; assignCharToCmd s)
 
   (* cree un fichier donner avec un nom choisi *)
   let writeFile() =
-    let fname = newfileName() in
-    let newFile = open_out fname in
-    output_string newFile (String.concat " " ["###";fname;"\n"]);
-    let set = ensembleFini () in
-    let baseExp = baseExpression set in
-
-    output_string newFile (String.concat "" [baseExp;"\n"]);
-    close_out newFile
+    fileName := newfileName();
+    newFile := open_out !fileName;
+    output_string !newFile (String.concat " " ["###";!fileName;"\n"]);
+    symbolSet := List.rev (ensembleFini());
+    let baseExp = baseExpression () in
+    output_string !newFile (String.concat "" [baseExp;"\n\n"]);
+    List.iter (interpretation) !symbolSet; (* lance interpretation sur tout les symboles *)
+    (* assign !symbolSet; *)
+    close_out !newFile
--- projet/save.mli
+++ projet/save.mli
@@ -2,8 +2,12 @@ val outputThenRead: string -> string
 
 val newfileName : unit -> string
 
-val baseExpression : char list -> string
+val ensembleFini : unit -> char list
 
-val checkListValid : 'a list -> 'a list -> bool
+val checkListValid : char list -> bool
+
+val baseExpression : unit -> string
+
+val interpretation : char -> unit 
 
 val writeFile : unit -> unit
